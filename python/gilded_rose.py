34d586e54187d03df14404d6b237a3ee01ed8c88
Rubén Pérez Herrando <rubb.phz@gmail.com>
2023-03-27 11:27:31 +0200

    refactor & solve mock test

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -8,26 +8,42 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             if item.name == "Aged Brie":
-                if item.quality < 50:
-                    item.quality += 1
+                self.update_aged_brie(item)
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality < 50:
-                    item.quality += 1
-                    if item.sell_in < 11:
-                        if item.quality < 50:
-                            item.quality += 1
-                    if item.sell_in < 6:
-                        if item.quality < 50:
-                            item.quality += 1
-                item.sell_in -= 1
-                if item.sell_in < 0:
-                    item.quality = 0
-            elif item.name != "Sulfuras, Hand of Ragnaros":
-                if item.quality > 0:
-                    item.quality -= 1
-                    item.sell_in -= 1
-                    if item.sell_in < 0 and item.quality > 0:
-                        item.quality -= 1
+                self.update_backstage_passes(item)
+            elif item.name == "Sulfuras, Hand of Ragnaros":
+                self.update_sulfuras(item)
+            else:
+                self.update_normal_items(item)
+        
+    def update_aged_brie(self, item):
+        if item.quality < 50:
+            item.quality += 1
+        item.sell_in -= 1
+        if item.sell_in < 0 and item.quality < 50:
+            item.quality += 1
+
+    def update_backstage_passes(self, item):
+        if item.sell_in <= 0:
+            item.quality = 0
+        elif item.sell_in <= 5:
+            item.quality += 3
+        elif item.sell_in <= 10:
+            item.quality += 2
+        else:
+            item.quality += 1
+        item.quality = min(item.quality, 50)
+        item.sell_in -= 1
+
+    def update_sulfuras(self, item):
+        pass
+
+    def update_normal_items(self, item):
+        if item.quality > 0:
+            item.quality -= 1
+        item.sell_in -= 1
+        if item.sell_in < 0 and item.quality > 0:
+            item.quality -= 1
 
 
 class Item:
